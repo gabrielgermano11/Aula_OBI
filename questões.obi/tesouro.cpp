79a8303f713cb160c8dd85b2c95c6b31c3efce85
gabrielgermano11 <gabrielgermano901@gmail.com>
2026-04-18 11:50:48 +0000

    tesouro.cpp

---
+++ questões.obi/tesouro.cpp
@@ -0,0 +1,12 @@
+//Questão da OBI 2020 Divisão do Tesouro//
+#include <iostream>
+using namespace std;
+int main(void){
+    int a, n, c;
+    cin >> a >> n;
+    
+    c = 2*a/(n + 2);
+    
+    cout << c << "\n";
+    return 0;
+}
